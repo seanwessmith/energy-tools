396fd9e91be8adff8a304123b1e9d84472e66c16
Sean Smith <sean.smith@paladin.insure>
2019-11-19 17:22:27 -0800

    init push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import StarterRouter from './routes/index';
+import './index.scss';
+import './reset.scss';
+
+const App = () => (
+  <Fragment>
+    <StarterRouter />
+  </Fragment>
+);
+
+export default App;
